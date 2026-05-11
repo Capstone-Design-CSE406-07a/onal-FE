27dcab6de1d15a569e1d8b18ff4e3c90713ed54a
youngju6143 <youngju6143@naver.com>
2026-05-12 00:22:15 +0900

    ✨ feat: Implement map view with heatmap and controls

--- src/pages/MapView.tsx
+++ src/pages/MapView.tsx
@@ -1,3 +1,16 @@
+import BottomControlsPanel from "../features/map/BottomControlsPanel";
+import MapHeatmap from "../features/map/MapHeatmap";
+import { MainFrame, Page } from "../features/map/styles";
+import TopInfoPanel from "../features/map/TopInfoPanel";
+
 export default function MapView() {
-  return <div>지도</div>;
+  return (
+    <Page data-node-id="42:1139">
+      <MainFrame>
+        <TopInfoPanel />
+        <MapHeatmap />
+        <BottomControlsPanel />
+      </MainFrame>
+    </Page>
+  );
 }

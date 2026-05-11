0bc86aab626f9ab36f0e47d879682edb19c1970e
youngju6143 <youngju6143@naver.com>
2026-05-12 00:22:52 +0900

    🐛 fix: Remove unused import of useLocation from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import routes from "./navigator/routes";
 import GlobalStyles from "./styles/GlobalStyles";
